46e40e514562342e849ceda1eb2a07a0d79b9641
Joss Dixon <josscdixon@gmail.com>
2022-05-03 14:02:55 +0100

    eslint

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Mollie",
-      animal: "Dog",
-      breed: "Lurcher",
-    }),
-    React.createElement(Pet, {
-      name: "Meg",
-      animal: "Dog",
-      breed: "Collie",
-    }),
-    React.createElement(Pet, {
-      name: "Miepje",
-      animal: "Dog",
-      breed: "Saluki",
-    }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Mollie" animal="dog" breed="Lurcher" />
+      <Pet name="Meg" animal="dog" breed="Collie" />
+      <Pet name="Miepje" animal="dog" breed="Saluki" />
+    </div>
+  )
+}
 
-render(React.createElement(App), document.getElementById("root"));
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Mollie",
+//       animal: "Dog",
+//       breed: "Lurcher",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Meg",
+//       animal: "Dog",
+//       breed: "Collie",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Miepje",
+//       animal: "Dog",
+//       breed: "Saluki",
+//     }),
+//   ]);
+// };
+
+render(<App />, document.getElementById("root"));
