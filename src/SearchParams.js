720476eef8405f111e32c48c280d10668e36f469
Joss Dixon <josscdixon@gmail.com>
2022-05-09 11:17:25 +0100

    options

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  const [location, setLocation] = useState("")
+  const [location, setLocation] = useState("");
+  const [animal, setAnimals] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +17,30 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((a) => {
+              return (
+                <option key={a} value={a}>
+                  {a}
+                </option>
+              )
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
